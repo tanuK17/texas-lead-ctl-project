d6d74f654e330b243026c499ec2ba388cc7eb35b
Tanush K <166859887+tanuK17@users.noreply.github.com>
2024-06-07 14:48:31 -0400

    Update scholarlymodule1.py

--- iterations of scholarly module/scholarlymodule1.py
+++ iterations of scholarly module/scholarlymodule1.py
@@ -1,3 +1,35 @@
+"""
+This iteration of code builds on the lsat one by incorporating proxies which help with avoiding Scholar's anti-bot measures. There's still an issue, though, that's getting thrown (see
+output here): [TLDR: Max Tries received, basically indicating that the proxy's time calibration may be an issue and potentially points to using ScraperAPI as a solution instead].
+
+Traceback (most recent call last):
+  File "/Users/tanushkaushik/Downloads/Texas Lead DS/scholarlymodule1.py", line 44, in <module>
+    papers = search_scholar(author, year, keyword)
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/Users/tanushkaushik/Downloads/Texas Lead DS/scholarlymodule1.py", line 10, in search_scholar
+    search_query = scholarly.search_pubs(f'{author} {year} {keyword}')
+                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/scholarly/_scholarly.py", line 160, in search_pubs
+    return self.__nav.search_publications(url)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/scholarly/_navigator.py", line 296, in search_publications
+    return _SearchScholarIterator(self, url)
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/scholarly/publication_parser.py", line 53, in __init__
+    self._load_url(url)
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/scholarly/publication_parser.py", line 59, in _load_url
+    self._soup = self._nav._get_soup(url)
+                 ^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/scholarly/_navigator.py", line 239, in _get_soup
+    html = self._get_page('https://scholar.google.com{0}'.format(url))
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/Library/Frameworks/Python.framework/Versions/3.12/lib/python3.12/site-packages/scholarly/_navigator.py", line 190, in _get_page
+    raise MaxTriesExceededException("Cannot Fetch from Google Scholar.")
+scholarly._proxy_generator.MaxTriesExceededException: Cannot Fetch from Google Scholar.
+
+"""
+
+
 from scholarly import scholarly, ProxyGenerator
 import json
 
